97dc9338441c612aab80906e412cacba7070a4e1
RafalKodland <168269766+RafalKodland@users.noreply.github.com>
2024-10-16 18:59:42 +0200

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from main import pswd
+import os
+import random
 
 # Zmienna intencje przechowuje uprawnienia bota
 intents = discord.Intents.default()
@@ -28,5 +30,13 @@ async def haha(ctx, num=2):
 @bot.command()
 async def password(ctx, len=10):
     await ctx.send(pswd(len))
+
+@bot.command()
+async def mem(ctx):
+    file = random.choice(os.listdir("memy"))
+    with open("memy\\" + file, "rb") as f:
+        obrazek = discord.File(f)
+    await ctx.send(file=obrazek)
+
 
 bot.run("")
